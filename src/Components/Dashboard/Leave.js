cf779b543ee9607f7521fe27bdd693dc2d0d9df1
developersweb507-arch <developersweb507@gmail.com>
2026-04-03 20:30:43 +0530

    updated leave

--- src/Components/Dashboard/Leave.js
+++ src/Components/Dashboard/Leave.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
-function Leave() {
+function App() {
   return (
-    <div>Leave</div>
-  )
+    <div
+      style={{
+        backgroundColor: "#f0f0f0",
+        padding: "20px",
+        textAlign: "center",
+        borderRadius: "8px",
+      }}
+    >
+      <h1 style={{ color: "darkblue", fontSize: "2rem" }}>
+        Hello, React with Inline CSS!
+      </h1>
+      <button
+        style={{
+          backgroundColor: "darkblue",
+          color: "white",
+          padding: "10px 20px",
+          border: "none",
+          borderRadius: "5px",
+          cursor: "pointer",
+        }}
+      >
+        Click Me
+      </button>
+    </div>
+  );
 }
 
-export default Leave
+export default App;
